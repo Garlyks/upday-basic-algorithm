d8a43ba710e2209f41c649ad7d2f7513929b1014
Jesus Rodriguez <jesus.rodriguez.in04@gmail.com>
2019-02-14 11:34:17 -0300

    minor test refactor

--- src/test/java/com/upday/AlgorithmTests.java
+++ src/test/java/com/upday/AlgorithmTests.java
@@ -6,47 +6,51 @@ import org.junit.Test;
 
 public class AlgorithmTests {
   @Test
-  public void algorithmTest() {
-    // [[1,2,[3]],4] -> [1,2,3,4].
-    Object[] input1 = { new Object[] { 1, 2, new Object[] { 3 } }, 4 };
-    Integer[] output1 = Algorithm.toOneDimension(input1);
-    assertEquals(4, output1.length);
-
-    // [9,[8,7],6,5,[[[[4,3,[2,1]]]
-    Object[] input2 = { 9, new Object[] { 8, 7 }, 6, 5,
-        new Object[] { new Object[] { new Object[] { 4, 3, new Object[] { 2, 1 } } } } };
-    Integer[] output2 = Algorithm.toOneDimension(input2);
-    assertEquals(9, output2.length);
-    
-    Object[] input3 = { 9, new Object[] { 8, 8 }, 8, 8};
-    Integer[] output3 = Algorithm.toOneDimension(input3);
-    assertEquals(5, output3.length);
+  public void singleDimensionArrayTest() {
+    assertEquals(
+        "Test single dimension array",
+        10,
+        Algorithm.toOneDimension(new Object[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 }).length);
   }
 
   @Test
-  public void algorithmBigSizeOnly3levelsTest() {
-    Long size = 100000l; 
+  public void multipleDimensionTest() {
+    Integer arraySize = 100;
+    assertEquals(
+        "Test multidimensional dimension array",
+        arraySize,
+        Algorithm.toOneDimension(getUnflattenAlgorithm(arraySize)));
+  }
+  
+  @Test
+  public void arrayWithNullValuesTest() {
+    assertEquals(
+        "Test with array with null values",
+        9,
+        Algorithm.toOneDimension(new Object[] { 1, null, 2, 3, new Object[] {null,4},5, 6, 7, 8, 9 }).length);
+  }
+  
+  @Test
+  public void emptyArrayTest() {
+    assertEquals(
+        "Test with array empty",
+        0,
+        Algorithm.toOneDimension(new Object[] {}).length);
+  }
+
+  private Object[] getUnflattenAlgorithm(Integer size) {
     Object[] array = new Object[size.intValue()];
-    long time = System.currentTimeMillis();
     for (int i = 0; i < size; i++) {
-      if(i%2 == 0) {
-        array[i] = new Object[] {i};
-      }else if(i%3==0) {
-        array[i] = new Object[] {new Object[] {1}};
-      }else {
+      if (i % 2 == 0) {
+        array[i] = new Object[] { i };
+      } else if (i % 3 == 0) {
+        array[i] = new Object[] { new Object[] { 1 } };
+      } else {
         array[i] = i;
       }
       array[i] = i;
     }
-    long duration = System.currentTimeMillis()-time;
-    System.out.println("Creation "+duration);
-    time = System.currentTimeMillis();
-    Integer[] output = Algorithm.toOneDimension(array);
-    duration = System.currentTimeMillis()-time;
-    System.out.println("Process:  "+duration);
-    System.out.println(output.length);
-    
-    assertEquals(size, output.length, 0);
+    return array;
   }
 
 }
